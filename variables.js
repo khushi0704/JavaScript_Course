075273c9f6a65bc48a18c9b112dee252c130430b
khushi0704 <khushi.jain0704@gmail.com>
2024-04-23 12:04:42 +0530

    var & values

---
+++ variables.js
@@ -0,0 +1,8 @@
+console.log("terminal of variables");
+/*
+ Starts with letter , _ , $ , 
+ cannot contain spaces 
+ reserved keywords cannot be named as variable names
+ JS is caseSensitive 
+ camelCase is followed in convention
+ */
